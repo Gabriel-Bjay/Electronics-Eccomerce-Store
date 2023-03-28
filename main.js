1476634245ef4fb46d5f36bde19570f74bbe260c
Bjay <jayanjay2018@gmail.com>
2023-03-28 09:38:24 +0300

    Adding of Javascript to the Html

---
+++ main.js
@@ -0,0 +1,12 @@
+let shopButton = document.querySelector(".shopButton")
+let images = document.querySelector(".phone-images") 
+
+shopButton.addEventListener("click",function(){
+    shopButton.style.backgroundColor = "green";
+    images.style.width = "200%"
+    
+
+});
+
+
+
