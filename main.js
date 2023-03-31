f59c09cb21ee5db3a9a17b99461d878494910477
Bjay <jayanjay2018@gmail.com>
2023-03-31 07:56:05 +0300

    Updated Contacting Options

--- main.js
+++ main.js
@@ -8,5 +8,13 @@ shopButton.addEventListener("click",function(){
 
 });
 
+const search = document.getElementsByClassName(`search`);
+const APIKEY = 'cabe4f9b4a90578285781adb4b1c07a0';
 
+search.addEventListener('click',function(){
+    fetch(``)
+    .then(data => data.json())
+    .then(console.log)
+    .catch (error => {})
+})
 
