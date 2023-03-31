f7325dff4abe413f95f4914cc1b477295cab47d5
Bjay <jayanjay2018@gmail.com>
2023-03-31 09:59:48 +0300

    Updates

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ let shopButton = document.querySelector(".shopButton")
 let images = document.querySelector(".phone-images") 
 
 shopButton.addEventListener("click",function(){
-    shopButton.style.backgroundColor = "green";
-    images.style.width = "200%"
+    for(i=0;i<shopButton.length;i++){
+        shopButton.style.backgroundColor = "green";
+        images.style.width = "200%"
+    }
+    
     
 
 });
